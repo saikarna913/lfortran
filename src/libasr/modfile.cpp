a47b852ce3885fa1e9ce1772f99f4a9c19c818f9
saikarna913 <pardheevsai913@gmail.com>
2024-12-11 15:37:54 +0530

    Update modfile.cpp

--- src/libasr/modfile.cpp
+++ src/libasr/modfile.cpp
@@ -1,11 +1,11 @@
 #include <string>
-
 #include <libasr/config.h>
 #include <libasr/asr_utils.h>
 #include <libasr/asr_verify.h>
 #include <libasr/modfile.h>
 #include <libasr/serialization.h>
 #include <libasr/bwriter.h>
+#include <iostream>
 
 namespace LCompilers {
 
@@ -14,48 +14,26 @@ const std::string lfortran_modfile_type_string = "LCompilers Modfile";
 inline void save_asr(const ASR::TranslationUnit_t &m, std::string& asr_string) {
     #ifdef WITH_LFORTRAN_BINARY_MODFILES
     BinaryWriter b;
-#else
+    #else
     TextWriter b;
-#endif
-    // Header
+    #endif
     b.write_string(lfortran_modfile_type_string);
     b.write_string(LFORTRAN_VERSION);
-
-    // AST section: Original module source code:
-    // Currently empty.
-    // Note: in the future we can save here:
-    // * A path to the original source code
-    // * Hash of the orig source code
-    // * AST binary export of it (this AST only changes if the hash changes)
-
-    // ASR section:
-
-    // Export ASR:
-    // Currently empty.
-
-    // Full ASR:
     b.write_string(serialize(m));
-
     asr_string = b.get_str();
 }
 
-// The save_modfile() and load_modfile() must stay consistent. What is saved
-// must be loaded in exactly the same order.
-
-/*
-    Saves the module into a binary stream.
-
-    That stream can be saved to a mod file by the caller.
-    The sections in the file/stream are saved using write_string(), so they
-    can be efficiently read by the loader and ignored if needed.
-
-    Comments below show some possible future improvements to the mod format.
-*/
 std::string save_modfile(const ASR::TranslationUnit_t &m) {
-    LCOMPILERS_ASSERT(m.m_symtab->get_scope().size()== 1);
+    if (m.m_symtab->get_scope().size() != 1) {
+        std::cerr << "Error: Modfile scope size is not 1" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
     for (auto &a : m.m_symtab->get_scope()) {
-        LCOMPILERS_ASSERT(ASR::is_a<ASR::Module_t>(*a.second));
-        if ((bool&)a) { } // Suppress unused warning in Release mode
+        if (!ASR::is_a<ASR::Module_t>(*a.second)) {
+            std::cerr << "Error: Expected ASR::Module_t" << std::endl;
+            std::cerr << "Modfile name: " << a.first << ", Modfile path: <Path to modfile>" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
     }
 
     std::string asr_string;
@@ -70,11 +48,11 @@ std::string save_pycfile(const ASR::TranslationUnit_t &m) {
 }
 
 inline void load_serialised_asr(const std::string &s, std::string& asr_binary) {
-#ifdef WITH_LFORTRAN_BINARY_MODFILES
+    #ifdef WITH_LFORTRAN_BINARY_MODFILES
     BinaryReader b(s);
-#else
+    #else
     TextReader b(s);
-#endif
+    #endif
     std::string file_type = b.read_string();
     if (file_type != lfortran_modfile_type_string) {
         throw LCompilersException("LCompilers Modfile format not recognized");
@@ -86,8 +64,7 @@ inline void load_serialised_asr(const std::string &s, std::string& asr_binary) {
     asr_binary = b.read_string();
 }
 
-ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s,
-        bool load_symtab_id, SymbolTable &symtab) {
+ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s, bool load_symtab_id, SymbolTable &symtab) {
     std::string asr_binary;
     load_serialised_asr(s, asr_binary);
     ASR::asr_t *asr = deserialize_asr(al, asr_binary, load_symtab_id, symtab);
@@ -95,12 +72,10 @@ ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s,
     return tu;
 }
 
-ASR::TranslationUnit_t* load_pycfile(Allocator &al, const std::string &s,
-        bool load_symtab_id) {
+ASR::TranslationUnit_t* load_pycfile(Allocator &al, const std::string &s, bool load_symtab_id) {
     std::string asr_binary;
     load_serialised_asr(s, asr_binary);
     ASR::asr_t *asr = deserialize_asr(al, asr_binary, load_symtab_id);
-
     ASR::TranslationUnit_t *tu = ASR::down_cast2<ASR::TranslationUnit_t>(asr);
     return tu;
 }
